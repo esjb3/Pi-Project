f2822528fcedecc10c6d3ef4698015b8a673a2f8
Ender <54820598+ender@users.noreply.github.com>
2023-04-14 00:48:21 +0000

    Create vision.py

---
+++ vision.py
@@ -0,0 +1,6 @@
+# load up YOLOv8n vision model
+# train model on X dataset for N epochs (X and N TBD)
+
+# function to check if an image has a car
+
+# function to check if the crosswalk is obstructed by anything
