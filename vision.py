647b8c80f0b04d6cb04c5d03270ba0daf3d9668f
sdroddy3 <130679702+sdroddy3@users.noreply.github.com>
2023-04-14 15:05:31 -0500

    Update vision.py

--- vision.py
+++ vision.py
@@ -2,8 +2,10 @@
 # train model on X dataset for N epochs (X and N TBD)
 
 # function to check if an image has a car
-
+def car():
 # function to check if the crosswalk is obstructed by anything
+def object():
+    if not 
     # if not car, alert
     # if car alert by sound or saying it is not safe to cross
     
