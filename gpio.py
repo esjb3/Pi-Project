c9c55bcdfbf96c6dffe97a3e1ae17019c745c7d4
sdroddy3 <130679702+sdroddy3@users.noreply.github.com>
2023-04-14 15:05:00 -0500

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -1,14 +1,16 @@
 # load gpio and pins for cam, speaker, light
-
+import GPIO.RPi as GPIO
 
 # function to turn on light
     # output to led pin
-
+def turnOn(light, pin):
 # function to play sound through speaker
     # play sound
-
+def playSound(sound, pin):
 # function to get video feed and recursively send to vision model
+def video(video, pin):
     # forever:
         # get data stream from camera output and output
+        output = 
         # this could be a generator
         
