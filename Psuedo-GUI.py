7f5a6a0c1a5bbae66c39a925313629e29d610d15
James Beene <jamesbeene44@gmail.com>
2023-04-13 19:38:22 -0500

    pseudo code for GUI

---
+++ Psuedo-GUI.py
@@ -0,0 +1,16 @@
+# collect data
+# function for cars that ignore light to test efficiency of light
+    # if car is detected by camera while stop light is on
+        # add to counter
+# function for average elapesed time that light stays on
+    # while light is on start timer
+    # when timer stops add the elapsed time to a list
+    # every time an elapsed time is added to a list, it calculates the average
+# function for amount of walkers
+    # if light turns on 
+        # add to counter
+
+# GUI class
+# display the data on right half of screen
+# Left half will alternate pictures of a person walking (if someone is walking) or an
+#  empty street (if no one is walking)
