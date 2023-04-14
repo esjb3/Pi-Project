4b07cc5dee07206e903e9c3161352099b9e16022
Ender <54820598+ender@users.noreply.github.com>
2023-04-14 00:54:03 +0000

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -1,7 +1,12 @@
 # load gpio and pins for cam, speaker, light
 
 # function to turn on light
+    # output to led pin
 
 # function to play sound through speaker
+    # play sound
 
 # function to get video feed and recursively send to vision model
+    # forever:
+        # get data stream from camera output and output
+        # this could be a generator
