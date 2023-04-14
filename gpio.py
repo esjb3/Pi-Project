e00621b64a125508e6db51d127c93f884bb6b115
Ender <54820598+ender@users.noreply.github.com>
2023-04-14 00:48:09 +0000

    Create gpio.py

---
+++ gpio.py
@@ -0,0 +1,7 @@
+# load gpio and pins for cam, speaker, light
+
+# function to turn on light
+
+# function to play sound through speaker
+
+# function to get video feed and recursively send to vision model
