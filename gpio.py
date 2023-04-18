f4e7f01f2dece723c10849ce64a4ef12500f4a3b
sdroddy3 <130679702+sdroddy3@users.noreply.github.com>
2023-04-17 20:24:54 -0500

    Create gpio.py

--- gpio.py
+++ gpio.py
@@ -1,16 +1,28 @@
 # load gpio and pins for cam, speaker, light
 import GPIO.RPi as GPIO
+GPIO.setmode(GPIO.BCM)
+def setGPIO():
+    gpio = [1]
+    GPIO.setup(gpio, GPIO.OUT)
+    
+    return gpio
+
+
+
 
 # function to turn on light
     # output to led pin
-def turnOn(light, pin):
+def turnOn(pin)
+    if nothing comes turn light on
+    
+    GPIO.output(gpio[pin], GPIO.HIGH)
 # function to play sound through speaker
     # play sound
 def playSound(sound, pin):
-# function to get video feed and recursively send to vision model
-def video(video, pin):
-    # forever:
-        # get data stream from camera output and output
-        output = 
-        # this could be a generator
+    if something is detected sound plays
+    
+    
+   
+GPIO.cleanup()
+
         
