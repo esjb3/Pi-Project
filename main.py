238b9c9a43ca1b67a9298dd2aac3248d4c77ca23
Ender <54820598+ender@users.noreply.github.com>
2023-04-21 18:20:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,70 @@
+import pygame
+import RPi.GPIO as GPIO
 from vision import Vision
 
+pygame.init()
+
+sounds = {
+    "obstruction": pygame.mixer.Sound("caution.mp3"),
+    "vehicle": pygame.mixer.Sound("stop.mp3"),
+    "clear": pygame.mixer.Sound("clear.mp3"),
+}
+
 class Main(Vision):
-    def __init__(self):
-        super().__init__("yolov8n", 0)
+    def __init__(self, debug=False):
+        super().__init__("yolov8n", 0, debug)
+        
+        self.led = 13
+        self.debug = debug
+
+        self.last = None
+
+    @property
+    def debug(self):
+        return self._debug
+    
+    @debug.setter
+    def debug(self, debug):
+        self._debug = debug
+
+    @property
+    def last(self):
+        return self._last
+    
+    @last.setter
+    def last(self, last):
+        self._last = last
 
     def process_frame(self):
         d = super().detect()
-        
-        if d["car"]:
-            # DO NOT GO ALERT TO WALKER
-            pass
+        print(d, self.last == "clear")
+        if d == "clear":
+            if self.last != "clear":
+                sounds["clear"].play()
+            self.last = "clear"
+            return
+
+        if d["vehicle"]:
+            sounds["vehicle"].play()
+            self.last = "vehicle"
         elif d["obstruction"]:
-            # POSSIBLE OBSTRUCTION ALERT TO WALKER
-            pass
+            sounds["obstruction"].play()
+            self.last = "obstruction"
         if d["person"]:
-            # FLASHING LIGHT TO INDICATE POSSIBLE
-            # PEDESTRIAN ON CROSSWALK TO ONCOMNIG TRAFFIC
-            pass
+            self.turnOn(LED)
+
+    def turnOn(self, pin):
+        GPIO.output(pin, True)
+
+    def turnOff(self, pin):
+        GPIO.output(pin, False)
 
     def start(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.led, GPIO.OUT)
+
         while True:
             self.process_frame()
 
-main = Main()
+main = Main(True)
 main.start()
